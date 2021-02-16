3dcb5d998ca5257d7294397f6061fc166ce6243f
Josiah Moore <jcm5155@gmail.com>
2021-02-16 02:20:06 -0600

    minor refactor to improve performance and readability; removed walrus operator for increased compatibility

--- python/next_biggest_number.py
+++ python/next_biggest_number.py
@@ -7,22 +7,18 @@ def main():
 
 
 def next_biggest_number(num):
-    list_num = list(str(num))
-    len_num = len(list_num)
-
-    # If the list is already in sorted reverse order, just return -1 immediately
-    if sorted(list_num)[::-1] == list_num:
-        return -1
-
-    # Iterate backwards through increasingly larger slices until we find a slice that is NOT in sorted reverse order
-    for idx in range(1, len_num):
-        cutoff = len_num - idx - 1
-        current_list_slice = list_num[cutoff:]
-        if current_list_slice != (tail_values := sorted(current_list_slice)[::-1]):
-            # Pop off the next largest number in relation to our slice's value at index 0
-            new_lead_tail_value = tail_values.pop(tail_values.index(current_list_slice[0])-1)
-            # Re-sort the remaining tail values and concat everything together
-            return int(''.join(list_num[:cutoff]) + new_lead_tail_value + ''.join(tail_values[::-1]))
+    str_num = str(num)
+    # Iterate backwards through increasingly larger slices...
+    for cutoff in range(len(str_num) - 2, -1, -1):
+        current_slice = list(str_num[cutoff:])
+        reverse_sorted_slice = sorted(current_slice, reverse=True)
+        # ...until we find a slice that is NOT in sorted reverse order
+        if current_slice != reverse_sorted_slice:
+            # Pop off the next highest number compared to the current cutoff value
+            new_cutoff_value = reverse_sorted_slice.pop(reverse_sorted_slice.index(current_slice[0])-1)
+            # Re-sort the reversed slice in the opposite direction and concat everything together
+            return int(f"{str_num[:cutoff]}{new_cutoff_value}{''.join(sorted(reverse_sorted_slice))}")
+    return -1
 
 
 if __name__ == "__main__":
